e196d62fb1a33b9e0bed2541ce8094a14152b084
KD Ironside <kdironside@gmail.com>
2012-07-30 23:40:39 -0500

    remove token from public repo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Geoapp::Application.config.secret_token = 'de32fcabade9e38db4c341e3dafa901a801a521cfe721cc10d18b4cc9ef5bdb507f940fd90d2920ae73f849d970447217db030c857fb3860a2556270006bac60'
+Geoapp::Application.config.secret_token = ' you secret token here '
